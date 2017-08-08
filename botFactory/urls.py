ed95a062da16b891a3fcea13007e0e272c18f4b8
ipriver <maxdrozdov18@gmail.com>
2017-08-08 17:25:34 +0300

    views cbv updates

--- botFactory/urls.py
+++ botFactory/urls.py
@@ -6,10 +6,8 @@ app_name = 'botFactory'
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
-    # url(r'^register/user_register/$', views.user_register,
-    #     name='user_register'),
     url(r'^sign_in/$', IndexView.as_view(), name='sign_in'),
     url(r'^logout/$', views.user_logout, name='sign_in'),
-    url(r'^add_new_bot/$', views.add_new_bot, name='add_new_bot'),
-    url(r'^del_bot/$', views.del_bot, name='del_bot'),
+    # url(r'^add_new_bot/$', IndexView.add_new_bot, name='add_new_bot'),
+    # url(r'^del_bot/$', IndexView.del_bot, name='del_bot'),
 ]
--- botFactory/views.py
+++ botFactory/views.py
@@ -6,32 +6,36 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from .models import Account, Bot
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import TemplateView
+from django.views.generic import (TemplateView, DetailView, CreateView, 
+UpdateView, DeleteView)
 
 
 
 class IndexView(TemplateView):
     template_name = 'botFactory/index.html'
-    context = {
-        'authenticated': False,
-        'form': None,
-    }
 
-    def get(self, request):
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['authenticated'] = False
+        context['form'] = None
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
         if request.user.is_authenticated:
-            self.context['message'] = 'Hello, ' + request.user.username
-            self.context['authenticated'] = True
-            self.context['form'] = AddBotForm()
+            context['message'] = 'Hello, ' + request.user.username
+            context['authenticated'] = True
+            context['form'] = AddBotForm()
             user = request.user
             account = Account.objects.get(user=user)
             bot_list = Bot.objects.filter(account=account)
-            self.context['bot_list'] = bot_list
+            context['bot_list'] = bot_list
         else:
-            self.context['authenticated'] = False
-            self.context['form'] = SignInForm()
-        return render(request, self.template_name, self.context)
+            context['authenticated'] = False
+            context['form'] = SignInForm()
+        return render(request, self.template_name, context)
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         form = SignInForm(request.POST)
         if form.is_valid():
             username = request.POST['login']
@@ -40,63 +44,80 @@ class IndexView(TemplateView):
         return HttpResponseRedirect('/')
 
 
+    # def add_new_bot(self, request):
+    #     form = AddBotForm(request.POST)
+    #     if form.is_valid():
+    #         channel_name = request.POST['channel_name']
+    #         user = request.user
+    #         account = Account.objects.get(user=user)
+    #         if account.bot_count < 5:
+    #             bot = Bot(account=account, channel_name=channel_name)
+    #             bot.bot_start()
+    #             account.bot_count += 1
+    #             account.save()
+    #             bot.save()
+    #     return redirect('botFactory:index')
+
+    # @csrf_exempt
+    # def del_bot(self, request):
+    #     if request.is_ajax:
+    #         user = request.user
+    #         account = Account.objects.get(user=user)
+    #         account.bot_count -= 1
+    #         bot_list = Bot.objects.filter(account=account)
+    #         Bot.objects.filter(id=bot_list[0].id).delete()
+    #         account.save()
+
+    #     return redirect('botFactory:index')
+
+
 class RegisterView(TemplateView):
     template_name = 'botFactory/register.html'
     form_class = RegForm
 
-    def get(self, request):
+    def get_context_data(self, **kwargs):
+        context = super(RegisterView, self).get_context_data(**kwargs)
+        return context
+
+    def get(self, request, *args, **kwargs):
         form = self.form_class(initial={'key': 'value'})
-        return render(request, self.template_name, {'form': form})
+        context = self.get_context_data(form=form)
+        return render(request, self.template_name, context)
 
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
-            login = request.POST['login']
-            email = request.POST['email']
-            password = request.POST['password']
-            re_password = request.POST['re_password']
+            login = request.POST.get('login')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            re_password = request.POST.get('re_password')
             if password == re_password:
                 user = User.objects.create_user(login, email, password)
                 user.save()
                 account = Account(user=user, bot_count=0)
                 account.save()
                 user_login(request, login, password)
+            else:
+                return redirect('botFactory:register')
         return redirect('botFactory:index')
 
 def user_logout(request):
     logout(request)
     return redirect('botFactory:index')
 
-
 def user_login(request, username, password):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
 
-def add_new_bot(request):
-    if request.method == 'POST':
-        form = AddBotForm(request.POST)
-        if form.is_valid():
-            channel_name = request.POST['channel_name']
-            user = request.user
-            account = Account.objects.get(user=user)
-            if account.bot_count < 5:
-                bot = Bot(account=account, channel_name=channel_name)
-                bot.bot_start()
-                account.bot_count += 1
-                account.save()
-                bot.save()
-    return redirect('botFactory:index')
+class BotDetails(DetailView):
+    pass
 
+class CreateBot(CreateView):
+    pass
 
-@csrf_exempt
-def del_bot(request):
-    if request.is_ajax:
-        user = request.user
-        account = Account.objects.get(user=user)
-        account.bot_count -= 1
-        bot_list = Bot.objects.filter(account=account)
-        Bot.objects.filter(id=bot_list[0].id).delete()
-        account.save()
+class DeleteBot(DeleteView):
+    pass
 
-    return redirect('botFactory:index')
+class UpdateBot(UpdateView):
+    pass
