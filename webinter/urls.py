6cf66742582381048713409dec8aa5a9fd34d204
ipriver <maxdrozdov18@gmail.com>
2017-09-05 14:55:12 +0300

    template auth check

--- webinter/urls.py
+++ webinter/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'webinter'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^ajax/(?P<name>\w*)', views.ajax, name='ajax'),
-    url(r'^json_ajax/', views.json_ajax, name='json_ajax'),
+    url(r'^logout/$', views.logout_view, name='logout'),
 ]
--- webinter/views.py
+++ webinter/views.py
@@ -1,46 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from django.views.decorators.csrf import csrf_protect
-import json
-import datetime
-import random
+from django.contrib.auth import logout
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-class Person:
-    def __init__(self, **kwargs):
-        self.name = kwargs['name'] + '!!!'
-        self.age = self.modify_age(kwargs['age'])
-        #self.date = self.cr_date()
-
-    def modify_age(self, age):
-        new_age = random.randint(1,100)
-        return new_age
-
-    def cr_date(self):
-        return datetime.datetime.now()
-    #def __str__(self):
-    #    return 'myobject: {} and {} date: {}'.format(self.name, self.age, self.date)
-
-def ajax(request, name=None):
-    print(name)
-    if request.method == 'POST' and request.is_ajax():
-        return HttpResponse('{} {} your name and password'.format(request.POST.get('name'), request.POST.get('password')))
-    name = request.GET.get('name', '')
-    if request.is_ajax():
-
-        return HttpResponse('{} name'.format(name))
-    return HttpResponse('{} your name'.format(name))
-
-def json_ajax(request):
-    if request.method == 'POST' and request.is_ajax():
-        data = json.loads(request.body)
-        print(data)
-        for key in data:
-            print('key: {}, data: {}'.format(key, data[key]))
-        pj = Person(**data)
-        print(pj)
-        print(json.dumps(pj.__dict__))
-        return HttpResponse(json.dumps(pj.__dict__))
+def logout_view(request):
+    logout(request)
+    return redirect('/')
