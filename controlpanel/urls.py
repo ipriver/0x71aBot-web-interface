caef4f444b11f1c63fc92d757af1a858305c34f2
ipriver <maxdrozdov18@gmail.com>
2017-09-18 18:51:57 +0300

    bot_start communication with bot service

--- controlpanel/urls.py
+++ controlpanel/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'controlpanel'
 urlpatterns = [
-
+    url(r'^start/$', views.start_bot, name='start_bot'),
 ]
--- controlpanel/views.py
+++ controlpanel/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import requests
+
+def start_bot(request):
+    user_channel = request.user.get_username()
+    response = requests.post('http://localhost:8080', 
+        json={"channel": request.user.get_username()})
+    return HttpResponse(response.status_code)
