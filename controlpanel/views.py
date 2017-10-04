a0c23eac8370e10b77d3ee8956da3b9b3685fc36
ipriver <maxdrozdov18@gmail.com>
2017-10-04 20:46:04 +0300

    Fix: json request and index cache disabled

--- controlpanel/views.py
+++ controlpanel/views.py
@@ -6,5 +6,5 @@ import requests
 def start_bot(request):
     user_channel = request.user.get_username()
     response = requests.post('http://localhost:8080', 
-        json={"channel": request.user.get_username()})
+        json={"user_id": request.user.id ,"channel": request.user.get_username()})
     return HttpResponse(response.status_code)
--- webinter/urls.py
+++ webinter/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.cache import cache_page
 
 app_name = 'webinter'
 urlpatterns = [
-    url(r'^$',  cache_page(60*60)(views.IndexView.as_view()), name='index'),
+    url(r'^$',  views.IndexView.as_view(), name='index'),
     url(r'^logout/$', views.logout_view, name='logout'),
 ]
