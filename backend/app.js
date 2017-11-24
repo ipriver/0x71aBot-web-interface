6cb3dd50048967cb37a420788416f5d93323e678
ipriver <maxdrozdov18@gmail.com>
2017-11-24 19:43:41 +0300

    fresh express.js

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+const port = process.env.PORT || '3000';
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
